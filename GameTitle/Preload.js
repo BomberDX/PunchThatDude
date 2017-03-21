8f2f5eb92f80f0ed62a87ed8b83ec97a6ba93256
Alantica <missionbit@missionbits-MacBook-Air-30.local>
2017-03-20 17:53:54 -0700

    Found assets

--- GameTitle/Preload.js
+++ GameTitle/Preload.js
@@ -6,15 +6,15 @@ preload.prototype = {
 		var loadingBar = this.add.sprite(160,240,"loading");
 		loadingBar.anchor.setTo(0.5,0.5);
 		
-		this.game.load.spritesheet("numbers","assets/numbers.png",100,100);
-		console.log("loaded numbers");
-		this.game.load.image("gametitle","assets/gametitle.png");
-		console.log("loaded gametitle");
+		this.game.load.spritesheet("Doge","assets/Doge_Sprite_Sheet.png",100,100);
+		console.log("Loaded Doge");
+		this.game.load.spritesheet("Jojo","assets/JotaroKid.png",100,100);
+		console.log("Loaded Doge");
 		this.game.load.image("play","assets/play.png");
 		console.log("loaded play");
-		this.game.load.image("higher","assets/higher.png");
-		console.log("loaded higher");
-		this.game.load.image("lower","assets/lower.png");
+		this.game.load.image("Platform","assets/platform.png");
+		console.log("loaded platform");
+		this.game.load.image("Cloudy","assets/CloudyBackground.png");
 		console.log("loaded lower");
 		this.game.load.image("gameover","assets/gameover.png");
 		console.log("loaded gameover");
