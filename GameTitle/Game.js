1fd6d9ff652f13fafa74475969a2bd6e627eafbf
EmiiLiHe <missionbit@Missions-MacBook-Air-6.local>
2017-04-10 16:16:34 -0700

    updated merge conflicts

--- GameTitle/Game.js
+++ GameTitle/Game.js
@@ -1,47 +1,14 @@
-<<<<<<< HEAD
-var theGame = function (game){
-    var cursors;
-    var stuff;
-    var speed = 6;
-=======
 var theGame = function(game){
 	score = 0;
     var gameJojo;
     var gameDoge;
     var cursors;
     
->>>>>>> origin/Sebas-Branch
 }
 
 theGame.prototype = {
     
 	create: function(){
-<<<<<<< HEAD
-        
-        this.stuff = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,"stuff");
-//        this.stuff.anchor.setTo(0.5,0.5);
-//        var stuff2 = this.game.add.sprite(60,60,"stuff2");
-//        stuff.anchor.setTo(0.5,0.5);
-        this.game.stage.backgroundColor = '#87CEEB';
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-        
-//        this.wasd = {
-//        up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
-//        down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
-//        left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
-//        right: this.game.input.keyboard.addKey(Phaser.Keyboard.D),
-//        };
-//       
-	},
-    update: function(){
-        if(this.cursors.right.isDown){
-//         if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-             console.log("right arrow pressed");
-            this.stuff.x += this.speed;
-        }
-    }
-};
-=======
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.world.setBounds(0,0,1200,600);
          var background = this.game.add.sprite(0,0,"background");
@@ -104,4 +71,3 @@ theGame.prototype = {
     }
     }
 }
->>>>>>> origin/Sebas-Branch
--- GameTitle/Preload.js
+++ GameTitle/Preload.js
@@ -5,18 +5,6 @@ preload.prototype = {
 		console.log("in preload of preload.js");
 		var loadingBar = this.add.sprite(160,240,"loading");
 		loadingBar.anchor.setTo(0.5,0.5);
-		
-<<<<<<< HEAD
-		this.game.load.image("stuff","assets/stuff.jpg");
-		console.log("loaded numbers");
-		this.game.load.image("gametitle","assets/gametitle.png");
-		console.log("loaded gametitle");
-		this.game.load.image("play","assets/play.png");
-		console.log("loaded play");
-		this.game.load.image("stuff2","assets/stuff2.png");
-		console.log("loaded higher");
-		this.game.load.image("lower","assets/lower.png");
-=======
 		this.game.load.spritesheet("Doge","assets/Doge_Sprite_Sheet.png",83,81,35);
         console.log("Loaded DogeSprite")
 		this.game.load.spritesheet("Jojo","assets/JotaroKid.png",125,95,51);
@@ -28,7 +16,6 @@ preload.prototype = {
 		this.game.load.image("Platform","assets/platform.png");
 		console.log("loaded platform");
 		this.game.load.image("Cloudy","assets/CloudyBackground.png");
->>>>>>> origin/Sebas-Branch
 		console.log("loaded lower");
 		this.game.load.image("gameover","assets/gameover.png");
 		console.log("loaded gameover");
