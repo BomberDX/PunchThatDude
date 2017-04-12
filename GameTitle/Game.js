a79c0d8490369579f2bf87c830b0a72ccc7eb3a6
Zecxal <missionbit@Missions-MacBook-Air-2.local>
2017-04-12 16:48:16 -0700

    left and right doge

--- GameTitle/Game.js
+++ GameTitle/Game.js
@@ -11,9 +11,9 @@ theGame.prototype = {
 	create: function(){
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.world.setBounds(0,0,1200,600);
-         var background = this.game.add.sprite(0,0,"FightBackG");
-                background.height=600;
-                background.width= 1200;
+        var background = this.game.add.sprite(0,0,"FightBackG");
+        background.height=600;
+        background.width= 1200;
         gameDoge = this.game.add.sprite(250,430,"Doge");
         gameDoge.anchor.setTo(0.5,0.5)
         gameJojo = this.game.add.sprite(950,430,"Jojo");
@@ -24,6 +24,8 @@ theGame.prototype = {
         
         gameDoge.body.collideWorldBounds = true;
         gameJojo.body.collideWorldBounds = true;
+        
+        
         cursors = this.game.input.keyboard.createCursorKeys();
         leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
         rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
@@ -32,46 +34,52 @@ theGame.prototype = {
 	},
     update:function() {
         
-    if (cursors.up.isDown){
-        gameJojo.body.velocity.y = -600;
-        
-    }
-    else if(cursors.down.isDown) {
-        gameJojo.body.velocity.y = 600;
+        if (cursors.up.isDown){
+            gameJojo.body.velocity.y = -600;
 
-    }
-    else if(cursors.left.isDown) {
-        gameJojo.scale.x*=-1;
-        gameJojo.body.velocity.x = -350;
+        }
+        else if(cursors.down.isDown) {
+            gameJojo.body.velocity.y = 600;
 
-    }
-    else if(cursors.right.isDown) {
-        gameJojo.scale.x*=-1;
-        gameJojo.body.velocity.x = 350;
-    }
-    else{
-        gameJojo.body.velocity.x = 0;
-        gameJojo.body.velocity.y = 0;
-    }
-//    if (cursors.W.isDown){
-//        gameDoge.body.velocity.y = -100
-//    }
-//    else{
-//        gameDoge.body.velocity.x = 0;
-//        gameDoge.body.velocity.y = 0;
-//    }
-    
-    if ( leftKey.justPressed() ){
-        gameDoge.body.velocity.x = -100;
-    }
-    else if (rightKey.justPressed){
-        gameDoge.body.velocity.x = 100;
-    }
-    else if (upKey.justPressed){
-        gameDoge.body.velocity.y = -100;
-    }
-    else if (downKey.justPressed){
-        gameDoge.body.velocity.y = 100;
-    }
+        }
+        else if(cursors.left.isDown) {
+            gameJojo.scale.x = -1;
+            gameJojo.body.velocity.x = -350;
+
+        }
+        else if(cursors.right.isDown) {
+            gameJojo.scale.x = 1;
+            gameJojo.body.velocity.x = 350;
+        }
+        else{
+            gameJojo.body.velocity.x = 0;
+            gameJojo.body.velocity.y = 0;
+        }
+    //    if (cursors.W.isDown){
+    //        gameDoge.body.velocity.y = -100
+    //    }
+    //    else{
+    //        gameDoge.body.velocity.x = 0;
+    //        gameDoge.body.velocity.y = 0;
+    //    }
+
+        if ( leftKey.justPressed() ){
+            gameDoge.scale.x = -1;
+            gameDoge.body.velocity.x = -350;
+        }
+        else if (rightKey.justPressed()){
+            gameDoge.scale.x = 1;
+            gameDoge.body.velocity.x = 350;
+        }
+        else if (upKey.justPressed()){
+            gameDoge.body.velocity.y = -600;
+        }
+        else if (downKey.justPressed()){
+            gameDoge.body.velocity.y = 600;
+        }
+//        else {
+//            gameDoge.body.velocity.x = 0;
+//            gameDoge.body.velocity.y = 0;
+//        }
     }
 }
