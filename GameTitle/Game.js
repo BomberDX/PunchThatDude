e5c21166cb6fd298b60dd6fd7788edc7f580bb64
Alantica <missionbit@missionbits-MacBook-Air-30.local>
2017-04-17 16:49:04 -0700

    git merge

--- GameTitle/Game.js
+++ GameTitle/Game.js
@@ -51,25 +51,51 @@ theGame.prototype = {
 
 	},
     update:function() {
-        listenForKeyPresses(gameJojo, this.cursors);
-        listenForKeyPresses(gameDoge, {
-            up: this.upKey,
-            down: this.downKey,
-            left: this.leftKey,
-            right: this.rightKey
-        });   
-    }
-}
-
-function listenForKeyPresses(gamePerson, cursors) {
-    if (cursors.up.isDown){
-        gamePerson.body.velocity.y = -600;
+        
+    if (this.cursors.up.isDown){
+        gameJojo.body.velocity.y = -600;
         
     }
-    else if(cursors.down.isDown) {
-        gamePerson.body.velocity.y = 600;
+    else if(this.cursors.down.isDown) {
+        gameJojo.body.velocity.y = 600;
 
         }
+        else if(this.cursors.left.isDown) {
+            gameJojo.scale.x = -1;
+            gameJojo.body.velocity.x = -350;
+
+        }
+        else if(this.cursors.right.isDown) {
+            gameJojo.scale.x = 1;
+            gameJojo.body.velocity.x = 350;
+        }
+        else{
+            gameJojo.body.velocity.x = 0;
+            gameJojo.body.velocity.y = 0;
+        }
+    //    if (cursors.W.isDown){
+    //        gameDoge.body.velocity.y = -100
+    //    }
+    //    else{
+    //        gameDoge.body.velocity.x = 0;
+    //        gameDoge.body.velocity.y = 0;
+    //    }
+        
+//        else {
+//            gameDoge.body.velocity.x = 0;
+//            gameDoge.body.velocity.y = 0;
+//        }
+
+
+        if ( this.leftKey.isDown ){
+            gameDoge.body.velocity.x = -350;
+            gameDoge.scale.x = -1;
+        }
+        else if (this.rightKey.isDown){
+            gameDoge.body.velocity.x = 350;
+            gameDoge.scale.x = 1;
+        }
+<<<<<<< HEAD
 <<<<<<< HEAD
         else if(cursors.left.isDown) {
             gamePerson.scale.x = -1;
@@ -88,11 +114,23 @@ function listenForKeyPresses(gamePerson, cursors) {
             gameJojo.scale.x = 1;
             gameJojo.body.velocity.x = 350;
 >>>>>>> master
+=======
+        else if (this.upKey.isDown){
+            console.log("UP KEY PRESSED");
+            gameDoge.body.velocity.y = -600;
+            console.log(gameDoge.body.velocity.y);
+        }
+        else if ( this.downKey.isDown){
+            console.log("DOWN KEY PRESSED");
+            gameDoge.body.velocity.y = 600;
+            console.log(gameDoge.body.velocity.y);
+>>>>>>> parent of 260b746... made stuff work totally me
         }
         else{
-            gamePerson.body.velocity.x = 0;
-            gamePerson.body.velocity.y = 0;
+            gameDoge.body.velocity.x = 0;
+            gameDoge.body.velocity.y = 0;
         }
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     //    if (cursors.W.isDown){
@@ -131,10 +169,15 @@ function listenForKeyPresses(gamePerson, cursors) {
             gameDoge.body.velocity.x = 0;
             gameDoge.body.velocity.y = 0;
         }
+=======
+>>>>>>> parent of 260b746... made stuff work totally me
 //        else if(!downKey.justPressed() && !upKey.justPressed() && !rightKey.justPressed() && !leftKey.justPressed()){
 //            gameDoge.body.velocity.x = 0;
 //            gameDoge.body.velocity.y = 0;
 //        }
     }
+<<<<<<< HEAD
 >>>>>>> master
+=======
+>>>>>>> parent of 260b746... made stuff work totally me
 }
