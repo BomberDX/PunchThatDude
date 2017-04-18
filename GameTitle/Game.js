161d01d7fb97fd9286fff18d9a2dded78d093644
momoxx44 <onamux@gmail.com>
2017-04-17 17:06:10 -0700

    testing jump

--- GameTitle/Game.js
+++ GameTitle/Game.js
@@ -52,7 +52,7 @@ theGame.prototype = {
 	},
     update:function() {
         
-    if (this.cursors.up.isDown){
+    if (this.cursors.up.isDown && this.gameJojo.body.touching.down){
         gameJojo.body.velocity.y = -600;
         
     }
@@ -60,12 +60,12 @@ theGame.prototype = {
         gameJojo.body.velocity.y = 600;
 
         }
-        else if(cursors.left.isDown) {
+        else if(this.cursors.left.isDown) {
             gameJojo.scale.x = -1;
             gameJojo.body.velocity.x = -350;
 
         }
-        else if(cursors.right.isDown) {
+        else if(this.cursors.right.isDown) {
             gameJojo.scale.x = 1;
             gameJojo.body.velocity.x = 350;
         }
